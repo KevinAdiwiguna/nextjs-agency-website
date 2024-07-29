07599366b62bc79fc30e05a838706c559d8d7754
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2024-07-29 13:37:22 +0800

    feat: active text when in a certain section

---
+++ src/hooks/use-active-section.ts
@@ -0,0 +1,29 @@
+"use client"
+import { useEffect, useState } from 'react';
+
+const useActiveSection = () => {
+    const [activeSection, setActiveSection] = useState<string>('');
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const sections = document.querySelectorAll('section');
+            let currentSection: string | null = '';
+            sections.forEach((section) => {
+                const sectionTop = section.offsetTop;
+                if (window.scrollY >= sectionTop - 60) {
+                    currentSection = section.getAttribute('id');
+                }
+            });
+            setActiveSection(currentSection || '');
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return activeSection;
+};
+
+export default useActiveSection;
