2caf37b14bc9ed6dfa11299587ec39dcb4f12d5e
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2024-07-29 13:41:22 +0800

    feat: constant data navigation

---
+++ src/constants/navigation.ts
@@ -0,0 +1,20 @@
+import {NAVIGATIONProps} from '@/types/navigation'
+
+export const NAVIGATION: NAVIGATIONProps[] = [
+    {
+        url: "",
+        name: "Home",
+    },
+    {
+        url: "#service",
+        name: "Service",
+    },
+    {
+        url: "#our-project",
+        name: "Our Project",
+    },
+    {
+        url: "#about-us",
+        name: "About Us",
+    }
+];
